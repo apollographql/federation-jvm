912fdc6d1925f9cf0b47a075a2c31f5529e1344b
Lenny Burdette <lenny@squareup.com>
2019-08-28 22:23:14 -0700

    feat: add federated tracing instrumentation
    Federated GraphQL services should include timing and error
    information as a Base64-encoded protocol buffer message in
    the "extensions.ftv1" field.
    
    This change includes a tracer that uses the graphql-java
    instrumentation API to record field timings and info and attach it
    on the execution context.
    
    I used the Apollo Server typescript code as reference:
    
    https://github.com/apollographql/apollo-server/blob/master/packages/apollo-engine-reporting/src/federatedExtension.ts
    https://github.com/apollographql/apollo-server/blob/master/packages/apollo-engine-reporting/src/treeBuilder.ts
    
    As well as my ruby implementation:
    
    https://github.com/lennyburdette/apollo-federation-ruby/blob/federated-tracing/lib/apollo-federation/tracing/tracer.rb
    
    Federated tracing documentation: https://www.apollographql.com/docs/apollo-server/federation/metrics/

---
+++ graphql-java-support/src/main/java/com/apollographql/federation/graphqljava/tracing/FederatedTracingInstrumentation.java
@@ -0,0 +1,235 @@
+package com.apollographql.federation.graphqljava.tracing;
+
+import com.google.protobuf.Timestamp;
+import graphql.ExecutionResult;
+import graphql.ExecutionResultImpl;
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+import graphql.execution.DataFetcherResult;
+import graphql.execution.ExecutionPath;
+import graphql.execution.ExecutionStepInfo;
+import graphql.execution.instrumentation.InstrumentationContext;
+import graphql.execution.instrumentation.InstrumentationState;
+import graphql.execution.instrumentation.SimpleInstrumentation;
+import graphql.execution.instrumentation.parameters.InstrumentationExecutionParameters;
+import graphql.execution.instrumentation.parameters.InstrumentationFieldFetchParameters;
+import mdg.engine.proto.Reports;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+import static graphql.execution.instrumentation.SimpleInstrumentationContext.whenCompleted;
+import static graphql.schema.GraphQLTypeUtil.simplePrint;
+
+public class FederatedTracingInstrumentation extends SimpleInstrumentation {
+    private static final String EXTENSION_KEY = "ftv1";
+    private final Options options;
+
+    private static final Logger logger = LoggerFactory.getLogger(FederatedTracingInstrumentation.class);
+
+    public FederatedTracingInstrumentation() {
+        this.options = Options.newOptions();
+    }
+
+    public FederatedTracingInstrumentation(Options options) {
+        this.options = options;
+    }
+
+    @Override
+    public InstrumentationState createState() {
+        return new FederatedTracingState();
+    }
+
+    @Override
+    public CompletableFuture<ExecutionResult> instrumentExecutionResult(ExecutionResult executionResult, InstrumentationExecutionParameters parameters) {
+        FederatedTracingState state = parameters.getInstrumentationState();
+
+        Map<Object, Object> extensions = executionResult.getExtensions();
+        Map<Object, Object> extensionsCopy = new LinkedHashMap<>(extensions == null ? Collections.emptyMap() : extensions);
+
+        Reports.Trace trace = state.toProto();
+
+        extensionsCopy.put(EXTENSION_KEY, Base64.getEncoder().encodeToString(trace.toByteArray()));
+
+        if (options.isDebuggingEnabled()) {
+            logger.debug(trace.toString());
+        }
+
+        return CompletableFuture.completedFuture(new ExecutionResultImpl(executionResult.getData(), executionResult.getErrors(), extensionsCopy));
+    }
+
+    @Override
+    public InstrumentationContext<Object> beginFieldFetch(InstrumentationFieldFetchParameters parameters) {
+        FederatedTracingState state = parameters.getInstrumentationState();
+
+        long startNanos = System.nanoTime();
+        return whenCompleted((result, throwable) -> {
+            long endNanos = System.nanoTime();
+
+            ExecutionStepInfo executionStepInfo = parameters.getEnvironment().getExecutionStepInfo();
+            state.addNode(
+                    executionStepInfo,
+                    // relative to the trace's start_time, in ns
+                    startNanos - state.getStartRequestNanos(),
+                    // relative to the trace's start_time, in ns
+                    endNanos - state.getStartRequestNanos(),
+                    convertErrors(throwable, result)
+            );
+        });
+    }
+
+    // Field resolvers can throw exceptions or add errors to the DataFetchingResult. This method normalizes them to a
+    // single list of GraphQLErrors.
+    private List<GraphQLError> convertErrors(Throwable throwable, Object result) {
+        ArrayList<GraphQLError> graphQLErrors = new ArrayList<>();
+
+        if (throwable != null) {
+            if (throwable instanceof GraphQLError) {
+                graphQLErrors.add((GraphQLError) throwable);
+            } else {
+                graphQLErrors.add(GraphqlErrorBuilder.newError()
+                        .message(throwable.getMessage())
+                        .build());
+            }
+        }
+
+        if (result instanceof DataFetcherResult && ((DataFetcherResult) result).hasErrors()) {
+            DataFetcherResult theResult = (DataFetcherResult) result;
+            graphQLErrors.addAll(theResult.getErrors());
+        }
+
+        return graphQLErrors;
+    }
+
+    /**
+     * Stores timing information and a map of nodes for field information.
+     */
+    private static class FederatedTracingState implements InstrumentationState {
+        private final Instant startRequestTime;
+        private final long startRequestNanos;
+        private final LinkedHashMap<ExecutionPath, Reports.Trace.Node.Builder> nodesByPath;
+
+        private FederatedTracingState() {
+            // record start time when creating instrumentation state for a request
+            startRequestTime = Instant.now();
+            startRequestNanos = System.nanoTime();
+
+            nodesByPath = new LinkedHashMap<>();
+            nodesByPath.put(ExecutionPath.rootPath(), Reports.Trace.Node.newBuilder());
+        }
+
+        Reports.Trace toProto() {
+            return Reports.Trace.newBuilder()
+                    .setStartTime(getStartTimestamp())
+                    .setEndTime(getNowTimestamp())
+                    .setDurationNs(getDuration())
+                    .setRoot(nodesByPath.get(ExecutionPath.rootPath()))
+                    .build();
+        }
+
+        /**
+         * Adds node to nodesByPath and recursively ensures that all parent nodes exist.
+         */
+        void addNode(ExecutionStepInfo stepInfo, long startFieldNanos, long endFieldNanos, List<GraphQLError> errors) {
+            ExecutionPath path = stepInfo.getPath();
+            Reports.Trace.Node.Builder parent = getParent(path);
+
+            Reports.Trace.Node.Builder node = parent.addChildBuilder()
+                    .setStartTime(startFieldNanos)
+                    .setEndTime(endFieldNanos)
+                    .setParentType(simplePrint(stepInfo.getParent().getUnwrappedNonNullType()))
+                    .setType(stepInfo.simplePrint())
+                    .setResponseName(stepInfo.getResultKey());
+
+            String originalFieldName = stepInfo.getField().getName();
+
+            // set originalFieldName only when a field alias was used
+            if (!originalFieldName.equals(stepInfo.getResultKey())) {
+                node.setOriginalFieldName(originalFieldName);
+            }
+
+            errors.forEach(error -> {
+                Reports.Trace.Error.Builder builder = node.addErrorBuilder().setMessage(error.getMessage());
+                error.getLocations().forEach(location -> builder.addLocationBuilder()
+                        .setColumn(location.getColumn())
+                        .setLine(location.getLine()));
+            });
+
+            nodesByPath.put(path, node);
+        }
+
+        Reports.Trace.Node.Builder getParent(ExecutionPath path) {
+            List<Object> pathParts = path.toList();
+            ExecutionPath parentPath = ExecutionPath.fromList(pathParts.subList(0, pathParts.size() - 1));
+
+            if (!nodesByPath.containsKey(parentPath)) {
+                // This recurses to support nested lists. It will eventually find the root node
+                // created in the constructor.
+                Reports.Trace.Node.Builder missingParent = getParent(parentPath).addChildBuilder();
+
+                // Missing parents are always list items, so we need to add the `index` field to them.
+                // There isn't an instrumentation hook for list items, but we need a node in the trace
+                // tree to contain the fields in the list item. This doesn't apply to scalar values, as
+                // `beginFieldFetch` isn't called for those list items.
+                if (parentPath.getLevel() > 0) {
+                    Object lastPathPart = parentPath.toList().get(parentPath.getLevel());
+                    if (lastPathPart instanceof Number) { // will always be true
+                        int index = ((Number) lastPathPart).intValue();
+                        missingParent.setIndex(index);
+                    }
+                }
+
+                nodesByPath.put(parentPath, missingParent);
+                return missingParent;
+            }
+
+            return nodesByPath.get(parentPath);
+        }
+
+        public long getStartRequestNanos() {
+            return startRequestNanos;
+        }
+
+        private Timestamp getStartTimestamp() {
+            return Timestamp.newBuilder()
+                    .setSeconds(startRequestTime.getEpochSecond())
+                    .setNanos(startRequestTime.getNano()).build();
+        }
+
+        private Timestamp getNowTimestamp() {
+            Instant now = Instant.now();
+            return Timestamp.newBuilder()
+                    .setSeconds(now.getEpochSecond())
+                    .setNanos(now.getNano())
+                    .build();
+        }
+
+        private long getDuration() {
+            return System.nanoTime() - startRequestNanos;
+        }
+    }
+
+    public static class Options {
+        private final boolean debuggingEnabled;
+
+        public Options(boolean debuggingEnabled) {
+            this.debuggingEnabled = debuggingEnabled;
+        }
+
+        public static Options newOptions() {
+            return new Options(false);
+        }
+
+        public boolean isDebuggingEnabled() {
+            return debuggingEnabled;
+        }
+    }
+}
