e9ddc01904ca77e3b437ae1ceabb5b64565b066e
Roman Kudryashov <rskudryashov@gmail.com>
2020-01-29 00:38:34 +0300

    test: read all resources with Unix newlines (#43)

--- graphql-java-support/src/test/java/com/apollographql/federation/graphqljava/TestUtils.java
+++ graphql-java-support/src/test/java/com/apollographql/federation/graphqljava/TestUtils.java
@@ -6,13 +6,13 @@ import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
 final class TestUtils {
-    private TestUtils() {
-    }
+
+    private static final String BASE_LINE_SEPARATOR = "\n";
 
     static String readResource(String name) {
         InputStream is = SchemaUtils.class.getResourceAsStream(name);
         assert is != null;
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+        return reader.lines().collect(Collectors.joining(BASE_LINE_SEPARATOR));
     }
 }
