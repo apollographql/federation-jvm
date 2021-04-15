12bea62a2c24d47b2130579cf78e0e556984f8f0
Sachin D. Shinde <sachin@apollographql.com>
2021-04-15 01:10:06 -0700

    Use @Import in AppTest to import configuration, and additionally check for GraphQL errors and federated tracing in the test

--- spring-example/src/test/java/com/apollographql/federation/springexample/AppTest.java
+++ spring-example/src/test/java/com/apollographql/federation/springexample/AppTest.java
@@ -6,6 +6,7 @@ import com.graphql.spring.boot.test.GraphQLTestTemplate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
@@ -16,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @RunWith(SpringRunner.class)
 @GraphQLTest
+@Import(App.class)
 public class AppTest {
     @Autowired
     private GraphQLTestTemplate graphqlTestTemplate;
@@ -24,8 +26,11 @@ public class AppTest {
     public void lookupPlanckProduct() throws IOException {
         final GraphQLResponse response = graphqlTestTemplate.postForResource("queries/LookupPlanckProduct.graphql");
         assertNotNull(response, "response should not have been null");
-        assertTrue(response.isOk(), "response should have been OK");
+        assertTrue(response.isOk(), "response should have been HTTP 200 OK");
+        response.assertThatNoErrorsArePresent();
         final int quantity = response.get("$.data._entities[0].quantity", Integer.class);
         assertEquals(8658, quantity, "Inventory contains 8658 Plancks");
+        final String federatedTrace = response.get("$.extensions.ftv1", String.class);
+        assertNotNull(federatedTrace, "response should not have had null federated trace");
     }
 }
