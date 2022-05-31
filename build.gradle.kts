5e119a0ae8152a893298ba597c3885411366150a
Dariusz Kuc <9501705+dariuszkuc@users.noreply.github.com>
2022-05-31 10:52:39 -0500

    [build] use new Sonatype host for relesaes (#202)
    Co-authored-by: Dariusz Kuc <dkuc@apollographql.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ tasks {
     nexusPublishing {
         repositories {
             sonatype {
+                nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
                 username.set(System.getenv("SONATYPE_NEXUS_USERNAME"))
                 password.set(System.getenv("SONATYPE_NEXUS_PASSWORD"))
                 stagingProfileId.set(System.getenv("COM_APOLLOGRAPHQL_PROFILE_ID"))
