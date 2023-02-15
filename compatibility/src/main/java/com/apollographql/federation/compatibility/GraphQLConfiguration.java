e4f1baf395953ff36257b163a06b726ff2b6c2f5
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2023-02-14 22:08:24 -0600

    update compatibility tests with latest schema changes

--- compatibility/src/main/java/com/apollographql/federation/compatibility/GraphQLConfiguration.java
+++ compatibility/src/main/java/com/apollographql/federation/compatibility/GraphQLConfiguration.java
@@ -1,6 +1,7 @@
 package com.apollographql.federation.compatibility;
 
 import com.apollographql.federation.compatibility.model.DeprecatedProduct;
+import com.apollographql.federation.compatibility.model.Inventory;
 import com.apollographql.federation.compatibility.model.Product;
 import com.apollographql.federation.compatibility.model.ProductResearch;
 import com.apollographql.federation.compatibility.model.User;
@@ -17,42 +18,45 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GraphQLConfiguration {
 
-    @Bean
-    public FederatedTracingInstrumentation federatedTracingInstrumentation() {
-      return new FederatedTracingInstrumentation();
-    }
+  @Bean
+  public FederatedTracingInstrumentation federatedTracingInstrumentation() {
+    return new FederatedTracingInstrumentation();
+  }
 
-    @Bean
-    public GraphQlSourceBuilderCustomizer federationTransform() {
-        return builder -> builder.schemaFactory((registry, wiring) ->
-                Federation.transform(registry, wiring)
-                        .fetchEntities(env ->
-                            env.<List<Map<String, Object>>>getArgument(_Entity.argumentName).stream().map(reference -> {
-                                final String typeName = (String)reference.get("__typename");
-                                return switch (typeName) {
-                                    case "DeprecatedProduct" -> DeprecatedProduct.resolveReference(reference);
-                                    case "Product" -> Product.resolveReference(reference);
-                                    case "ProductResearch" -> ProductResearch.resolveReference(reference);
-                                    case "User" -> User.resolveReference(reference);
-                                    default -> null;
-                                };
-                            }).collect(Collectors.toList())
-                        )
-                        .resolveEntityType(env -> {
-                            final Object src = env.getObject();
-                            if (src instanceof DeprecatedProduct) {
-                                return env.getSchema().getObjectType("DeprecatedProduct");
-                            } else if (src instanceof Product) {
-                                return env.getSchema().getObjectType("Product");
-                            } else if (src instanceof ProductResearch) {
-                                return env.getSchema().getObjectType("ProductResearch");
-                            } else if (src instanceof User) {
-                                return env.getSchema().getObjectType("User");
-                            } else {
-                                return null;
-                            }
-                        })
-                        .build()
-        );
-    }
+  @Bean
+  public GraphQlSourceBuilderCustomizer federationTransform() {
+    return builder -> builder.schemaFactory((registry, wiring) ->
+      Federation.transform(registry, wiring)
+        .fetchEntities(env ->
+          env.<List<Map<String, Object>>>getArgument(_Entity.argumentName).stream().map(reference -> {
+            final String typeName = (String) reference.get("__typename");
+            return switch (typeName) {
+              case "DeprecatedProduct" -> DeprecatedProduct.resolveReference(reference);
+              case "Product" -> Product.resolveReference(reference);
+              case "ProductResearch" -> ProductResearch.resolveReference(reference);
+              case "User" -> User.resolveReference(reference);
+              case "Inventory" -> Inventory.resolveReference(reference);
+              default -> null;
+            };
+          }).collect(Collectors.toList())
+        )
+        .resolveEntityType(env -> {
+          final Object src = env.getObject();
+          if (src instanceof DeprecatedProduct) {
+            return env.getSchema().getObjectType("DeprecatedProduct");
+          } else if (src instanceof Product) {
+            return env.getSchema().getObjectType("Product");
+          } else if (src instanceof ProductResearch) {
+            return env.getSchema().getObjectType("ProductResearch");
+          } else if (src instanceof User) {
+            return env.getSchema().getObjectType("User");
+          } else if (src instanceof Inventory) {
+            return env.getSchema().getObjectType("Inventory");
+          } else {
+            return null;
+          }
+        })
+        .build()
+    );
+  }
 }
