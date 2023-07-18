3b87b30782b1865ea2332d082f7952a300e3beaa
Dariusz Kuc <9501705+dariuszkuc@users.noreply.github.com>
2023-07-18 15:49:49 -0500

    breaking: update to graphql-java v21 and Java 11 baseline (#332)
    This is a breaking change as `graphql-java` v21 requires Java 11.

--- compatibility/build.gradle.kts
+++ compatibility/build.gradle.kts
@@ -1,8 +1,8 @@
 import java.util.Properties
 
 plugins {
-    id("org.springframework.boot") version "3.0.6"
-    id("io.spring.dependency-management") version "1.1.0"
+    id("org.springframework.boot") version "3.1.1"
+    id("io.spring.dependency-management") version "1.1.2"
     java
 }
 
