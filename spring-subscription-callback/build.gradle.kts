a62363f88b4b3571988331f00b42c278bdf0ba4e
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2023-10-04 13:27:28 -0500

    cleanup dependencies

--- spring-subscription-callback/build.gradle.kts
+++ spring-subscription-callback/build.gradle.kts
@@ -6,23 +6,25 @@ plugins {
 
 val annotationsVersion: String by project
 val graphQLJavaVersion: String by project
+val mockWebServerVersion: String by project
 val slf4jVersion: String by project
 val springBootVersion: String by project
 val springGraphQLVersion: String by project
+val reactorVersion: String by project
 dependencies {
     compileOnly("org.jetbrains", "annotations", annotationsVersion)
-    api("com.graphql-java", "graphql-java", graphQLJavaVersion)
-    api("org.slf4j", "slf4j-api", slf4jVersion)
+    implementation("com.graphql-java", "graphql-java", graphQLJavaVersion)
+    implementation("org.slf4j", "slf4j-api", slf4jVersion)
     implementation("org.springframework.boot", "spring-boot-starter-graphql", springBootVersion)
     implementation("org.springframework.boot", "spring-boot-starter-web", springBootVersion)
     implementation("org.springframework.boot", "spring-boot-starter-webflux", springBootVersion)
-    testCompileOnly("org.jetbrains:annotations:$annotationsVersion")
+    testCompileOnly("org.jetbrains", "annotations", annotationsVersion)
     testImplementation(project(":federation-graphql-java-support"))
-    testImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
+    testImplementation("com.squareup.okhttp3", "mockwebserver", mockWebServerVersion)
     testImplementation("org.springframework.boot", "spring-boot-starter-test", springBootVersion)
     testImplementation("org.springframework.boot", "spring-boot-starter-websocket", springBootVersion)
     testImplementation("org.springframework.graphql", "spring-graphql-test", springGraphQLVersion)
-    testImplementation("io.projectreactor:reactor-test:3.5.9")
+    testImplementation("io.projectreactor", "reactor-test", reactorVersion)
 
 }
 
