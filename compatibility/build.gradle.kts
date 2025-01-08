2820bb4f899697d80e8338d19d694b3ad4a2a4b5
Dariusz Kuc <9501705+dariuszkuc@users.noreply.github.com>
2025-01-08 17:50:05 -0600

    chore: update dependencies (#426)
    Update dependencies:
    * graphql-java 22.1 -> 22.3
    * slf4j 2.0.13 -> 2.0.16
    * spring boot (for subscription callbacks) 3.3.0 -> 3.3.6
    
    Update test dependencies:
    * junit 5.10.2 -> 5.10.5
    * reactor test 3.6.6 -> 3.6.12
    * spring graphql test 1.3.0 -> 1.3.3

--- compatibility/build.gradle.kts
+++ compatibility/build.gradle.kts
@@ -1,8 +1,8 @@
 import java.util.Properties
 
 plugins {
-    id("org.springframework.boot") version "3.3.0"
-    id("io.spring.dependency-management") version "1.1.5"
+    id("org.springframework.boot") version "3.3.6"
+    id("io.spring.dependency-management") version "1.1.7"
     java
 }
 
