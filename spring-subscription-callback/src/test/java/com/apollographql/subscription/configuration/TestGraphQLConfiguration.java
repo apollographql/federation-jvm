e4e51281b3e3ecaaaab2722d6bcfac9cc29b8b5d
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2025-04-09 17:48:11 -0500

    cleanup config

--- spring-subscription-callback/src/test/java/com/apollographql/subscription/configuration/TestGraphQLConfiguration.java
+++ spring-subscription-callback/src/test/java/com/apollographql/subscription/configuration/TestGraphQLConfiguration.java
@@ -1,31 +1,18 @@
 package com.apollographql.subscription.configuration;
 
-import com.apollographql.federation.graphqljava.Federation;
-import com.apollographql.federation.graphqljava._Entity;
-import graphql.schema.DataFetcher;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import org.springframework.boot.autoconfigure.graphql.GraphQlSourceBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.graphql.execution.ClassNameTypeResolver;
+import org.springframework.graphql.data.federation.FederationSchemaFactory;
 
 public class TestGraphQLConfiguration {
-  // configure federation
-  @Bean
-  public GraphQlSourceBuilderCustomizer federationTransform() {
-    DataFetcher<?> entityDataFetcher =
-        env -> {
-          List<Map<String, Object>> representations = env.getArgument(_Entity.argumentName);
-          return representations.stream().map(representation -> null).collect(Collectors.toList());
-        };
 
-    return builder ->
-        builder.schemaFactory(
-            (registry, wiring) ->
-                Federation.transform(registry, wiring)
-                    .fetchEntities(entityDataFetcher)
-                    .resolveEntityType(new ClassNameTypeResolver())
-                    .build());
+  @Bean
+  public GraphQlSourceBuilderCustomizer customizer(FederationSchemaFactory factory) {
+    return builder -> builder.schemaFactory(factory::createGraphQLSchema);
+  }
+
+  @Bean
+  public FederationSchemaFactory federationSchemaFactory() {
+    return new FederationSchemaFactory();
   }
 }
